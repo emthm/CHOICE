601557d7f887d21957fb3cccebec4eac39c9e7b2
emthm <120452383+emthm@users.noreply.github.com>
2024-08-19 15:36:52 +0200

    Fixed bugs for building the documentation

--- choice/__init__.py
+++ choice/__init__.py
@@ -1,1 +1,6 @@
 from choice.choice_main import CHOICE
+from .choice_aux import *
+from .choice_data import *
+from .choice_interf import *
+from .choice_physics import *
+from .choice_read_and_write import *
